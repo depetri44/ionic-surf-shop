2c056ecb4ec76394e6ca83286026baa20f0e9c5a
depetri44 <120295465+depetri44@users.noreply.github.com>
2022-12-11 02:47:05 -0300

    1

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,13 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: ""
+    apiKey: "AIzaSyCzmGMFMQQ6yeE_A81Si5Ze7fubOaFj9uQ",
+    authDomain: "bahiashop-d0569.firebaseapp.com",
+    projectId: "bahiashop-d0569",
+    storageBucket: "bahiashop-d0569.appspot.com",
+    messagingSenderId: "600811398864",
+    appId: "1:600811398864:web:4636ee1af8e1e5f681e0d5",
+    measurementId: "G-1H4LLLPGBX"
   }
 };
 
